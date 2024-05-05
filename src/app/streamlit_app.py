5ab6ada82ea272c600c99a97fca90f188215264b
kenta-sugitani <72644951+kenta-sugitani@users.noreply.github.com>
2024-05-05 11:09:37 +0900

    Update streamlit_app.py

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import streamlit as st
+# import streamlit as st
 
-if st.button('Say hello'):
-    st.write('Hello World!')
+# if st.button('Say hello'):
+#     st.write('Hello World!')
+
+x = 10
+'x: ', x
