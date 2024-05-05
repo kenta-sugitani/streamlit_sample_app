07869e4e6509769a2151954969dc1835daba0a33
kenta-sugitani <72644951+kenta-sugitani@users.noreply.github.com>
2024-05-05 11:44:12 +0900

    Update streamlit_app.py

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -17,11 +17,17 @@ def convert_number(num):
     hexadecimal = hex(num).replace("0x", "")
     return binary, hexadecimal
 
+def binary_to_4bit(binary):
+    while len(binary) % 4 != 0:
+        binary = '0' + binary
+    return ' '.join([binary[i:i+4] for i in range(0, len(binary), 4)])
+
 st.title('10進数から2進数と16進数への変換')
 
 dec_num = st.number_input('10進数を入力してください', min_value=0, value=0, step=1)
 
 if st.button('変換'):
     binary, hexadecimal = convert_number(dec_num)
-    st.write(f'2進数: {binary}')
+    binary_4bit = binary_to_4bit(binary)
+    st.write(f'2進数: {binary_4bit}')
     st.write(f'16進数: {hexadecimal}')
